1d2107e3f13e3c7ce074fbc0446a02fe59ef9e8a
Leonard87ce <91459304+Leonard87ce@users.noreply.github.com>
2021-10-21 21:46:06 -0500

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const express = require("express");
 const bodyParser = require("body-parser");
-const repository = require("./repository");
-const app = express();
-const port = process.env.PORT || 3000;
+const repository = require("./repository")
+const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
